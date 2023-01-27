6ee9fea1030ae6dc5f0c089dc23de26cd21b56c3
Daniela Romero <danyrp45@gmail.com>
2023-01-27 12:20:12 -0600

    add flexLayout

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
 
@@ -12,6 +13,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
   declarations: [],
   exports: [
     MatButtonModule,
+    MatCardModule,
     MatIconModule,
     MatListModule,
     MatSidenavModule,
