ed01f34ad6a423e9f6b222ae13d11d956ecbf2e8
Daniela Romero <danyrp45@gmail.com>
2023-02-01 14:45:59 -0600

    implements autocomplete

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -1,21 +1,32 @@
 import { Component } from '@angular/core';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 import { Heroe } from '../../interfaces/heroes.interface';
 import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-buscar',
   templateUrl: './buscar.component.html',
-  styleUrls: ['./buscar.component.css']
+  styleUrls: ['./buscar.component.css'],
 })
 export class BuscarComponent {
-  termino : string = '';
-  heroes : Heroe[] = [];
+  termino: string = '';
+  heroes: Heroe[] = [];
+  heroeSeleccionado!: Heroe;
 
-  constructor(private heroesService: HeroesService){}
+  constructor(private heroesService: HeroesService) {}
 
-  buscando(){
-    this.heroesService.getHeroes().subscribe(heroes=>{
+  buscando() {
+    this.heroesService.getSugerencias(this.termino).subscribe((heroes) => {
       this.heroes = heroes;
-    })
+    });
+  }
+
+  opcionSeleccionada(evento: MatAutocompleteSelectedEvent) {
+    const heroe: Heroe = evento.option.value;
+    this.termino = heroe.superhero;
+    this.heroesService.getHeroePorId(heroe.id).subscribe((heroe) => {
+      this.heroeSeleccionado = heroe;
+    });
+    
   }
 }
