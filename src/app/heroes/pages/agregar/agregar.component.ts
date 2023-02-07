071183bd29d7196e8bf0b2e8e86e39ec5bf0e2d8
Daniela Romero <danyrp45@gmail.com>
2023-02-07 12:03:11 -0600

    edit service

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Heroe, Publisher } from '../../interfaces/heroes.interface';
 import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
   styleUrls: ['./agregar.component.css'],
 })
-export class AgregarComponent {
+export class AgregarComponent implements OnInit {
   publishers = [
     {
       id: 'Dc Comics',
@@ -26,12 +28,33 @@ export class AgregarComponent {
     publisher: Publisher.DCComics,
     alt_img: '',
   };
-  constructor(private heroesService: HeroesService) {}
+  constructor(
+    private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
+  ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((data) => {
+      let id = data.get('id');
+      if (id) {
+        this.heroesService.getHeroePorId(id).subscribe((heroe) => {
+          this.heroe = heroe;
+        });
+      }
+    });
+  }
 
   guardar() {
     if (this.heroe.superhero.trim().length === 0) return;
-    this.heroesService.agregarHeroe(this.heroe).subscribe((heroe) => {
-      console.log('heroe', heroe);
-    });
+
+    if (this.heroe.id) {
+      this.heroesService
+        .actualizarHeroe(this.heroe)
+        .subscribe((heroe) => console.log('Actualizando', heroe));
+    } else {
+      this.heroesService.agregarHeroe(this.heroe).subscribe((heroe) => {
+        this.router.navigate(['/heroes/editar', heroe.id]);
+      });
+    }
   }
 }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -23,4 +23,7 @@ export class HeroesService {
   agregarHeroe(heroe: Heroe):Observable<Heroe>{
     return this.http.post<Heroe>(`${this.baseUrl}/heroes`, heroe)
   }
+  actualizarHeroe(heroe: Heroe):Observable<Heroe>{
+    return this.http.put<Heroe>(`${this.baseUrl}/heroes/${heroe.id}`, heroe)
+  }
 }
